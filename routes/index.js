66b68b9a40f194099414fb2fd44759dc0de56bea
Hataipan Weerapin <nutpedd23@hotmail.com>
2019-07-13 16:11:57 +0700

    แงเป็นJS

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var connect = require("../config/database");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'GOT7' });
+  var query = "SELECT * FROM positions";
+  connect.query(query,function(error,result){
+    res.json(result)
+  })
 });
 
 
